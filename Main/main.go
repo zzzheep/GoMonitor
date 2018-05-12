29d45e97b824504d2e25683aa6ffe0ed72573cfc
刘望晨 <373393056@qq.com>
2018-05-12 16:18:37 +0800

    fsdfsd

--- Main/main.go
+++ Main/main.go
@@ -18,7 +18,6 @@ func main() {
 
 func collet() {
 	v, _ := mem.VirtualMemory()
-	c, _ := cpu.Info()
 	cc, _ := cpu.Percent(time.Second, false)
 	d, _ := disk.Usage("/")
 	n, _ := host.Info()
@@ -26,14 +25,6 @@ func collet() {
 	boottime, _ := host.BootTime()
 	btime := time.Unix(int64(boottime), 0).Format("2006-01-02 15:04:05")
 	fmt.Printf("Mem       	: %v MB  Free: %v MB Used:%v Usage:%f%%\n", v.Total/1024/1024, v.Available/1024/1024, v.Used/1024/1024, v.UsedPercent)
-
-	if len(c) > 0 {
-		for _, sub_cpu := range c {
-			modelname := sub_cpu.ModelName
-			cores := sub_cpu.Cores
-			fmt.Printf("CPU       	: %v   %v cores \n", modelname, cores)
-		}
-	}
 	fmt.Printf("Network   	: %v bytes / %v bytes\n", nv[0].BytesRecv, nv[0].BytesSent)
 	fmt.Printf("SystemBoot	: %v\n", btime)
 	fmt.Printf("CPU Used  	: used %f%% \n", cc[0])
