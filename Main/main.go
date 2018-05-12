5548c912fc43d8db38688b972d886849d22a29b9
刘望晨 <373393056@qq.com>
2018-05-12 16:12:06 +0800

    testcpu

--- Main/main.go
+++ Main/main.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/shirou/gopsutil/process"
-
 	"github.com/shirou/gopsutil/cpu"
 	"github.com/shirou/gopsutil/disk"
 	"github.com/shirou/gopsutil/host"
@@ -27,29 +25,19 @@ func collet() {
 	nv, _ := net.IOCounters(true)
 	boottime, _ := host.BootTime()
 	btime := time.Unix(int64(boottime), 0).Format("2006-01-02 15:04:05")
-	p, _ := process.Processes()
 	fmt.Printf("Mem       	: %v MB  Free: %v MB Used:%v Usage:%f%%\n", v.Total/1024/1024, v.Available/1024/1024, v.Used/1024/1024, v.UsedPercent)
-	if len(c) > 1 {
+
+	if len(c) > 0 {
 		for _, sub_cpu := range c {
 			modelname := sub_cpu.ModelName
 			cores := sub_cpu.Cores
 			fmt.Printf("CPU       	: %v   %v cores \n", modelname, cores)
 		}
-	} else {
-		sub_cpu := c[0]
-		modelname := sub_cpu.ModelName
-		cores := sub_cpu.Cores
-		fmt.Printf("CPU       	: %v   %v cores \n", modelname, cores)
 	}
-	fmt.Println()
 	fmt.Printf("Network   	: %v bytes / %v bytes\n", nv[0].BytesRecv, nv[0].BytesSent)
 	fmt.Printf("SystemBoot	: %v\n", btime)
 	fmt.Printf("CPU Used  	: used %f%% \n", cc[0])
 	fmt.Printf("HD        	: %v GB  Free: %v GB Usage:%f%%\n", d.Total/1024/1024/1024, d.Free/1024/1024/1024, d.UsedPercent)
 	fmt.Printf("OS        	: %v(%v)   %v  \n", n.Platform, n.PlatformFamily, n.PlatformVersion)
 	fmt.Printf("Hostname  	: %v  \n", n.Hostname)
-	for _, pchild := range p {
-		percent, err := pchild.MemoryPercent()
-		fmt.Println(pchild.Pid, "   ", percent, "%", " error:", err)
-	}
 }
