8513b9519253754b5192ec211dda29ced99c4b5f
liuwangchen <liuwangchen@ciphertrading.com>
2018-05-16 17:58:15 +0800

    gin

--- Main/main.go
+++ Main/main.go
@@ -1,48 +1,24 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"net/http"
 
-	"github.com/shirou/gopsutil/process"
-
-	"github.com/shirou/gopsutil/cpu"
-	"github.com/shirou/gopsutil/disk"
-	"github.com/shirou/gopsutil/host"
-	"github.com/shirou/gopsutil/mem"
-
-	"github.com/shirou/gopsutil/net"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	collet()
-}
-
-func collet() {
-	v, _ := mem.VirtualMemory()
-	c, _ := cpu.Info()
-	cc, _ := cpu.Percent(time.Second, false)
-	d, _ := disk.Usage("/")
-	n, _ := host.Info()
-	nv, _ := net.IOCounters(true)
-	boottime, _ := host.BootTime()
-	btime := time.Unix(int64(boottime), 0).Format("2006-01-02 15:04:05")
-	p, _ := process.Processes()
-	fmt.Printf("Mem       	: %v MB  Free: %v MB Used:%v Usage:%f%%\n", v.Total/1024/1024, v.Available/1024/1024, v.Used/1024/1024, v.UsedPercent)
-	for _, sub_cpu := range c {
-		modelname := sub_cpu.ModelName
-		cores := sub_cpu.Cores
-		fmt.Printf("CPU       	: %v   %v cores \n", modelname, cores)
-	}
-	fmt.Printf("Network   	: %v bytes / %v bytes\n", nv[0].BytesRecv, nv[0].BytesSent)
-	fmt.Printf("SystemBoot	: %v\n", btime)
-	fmt.Printf("CPU Used  	: used %f%% \n", cc[0])
-	fmt.Printf("HD        	: %v GB  Free: %v GB Usage:%f%%\n", d.Total/1024/1024/1024, d.Free/1024/1024/1024, d.UsedPercent)
-	fmt.Printf("OS        	: %v(%v)   %v  \n", n.Platform, n.PlatformFamily, n.PlatformVersion)
-	fmt.Printf("Hostname  	: %v  \n", n.Hostname)
-	for _, pchild := range p {
-		percent, err := pchild.MemoryPercent()
-		fmt.Println(pchild.Pid, "   ", percent, "%", " error:", err)
-	}
-	fmt.Println("new branche")
+	gin.SetMode(gin.DebugMode)
+	r := gin.Default()
+	r.GET("/monitor", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "monitorservice",
+		})
+	})
+	r.GET("/monitor/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.JSON(http.StatusOK, gin.H{
+			"name": name,
+		})
+	})
+	r.Run()
 }
