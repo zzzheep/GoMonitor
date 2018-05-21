6dfc674f5af388e07d3d9e22daf4d3c0391efd94
liuwangchen <liuwangchen@ciphertrading.com>
2018-05-21 18:06:39 +0800

    websockettest

--- Main/main.go
+++ Main/main.go
@@ -1,10 +1,44 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 )
+
+var wsupgrader = websocket.Upgrader{
+	ReadBufferSize:   1024,
+	WriteBufferSize:  1024,
+	HandshakeTimeout: 5 * time.Second,
+	// 取消ws跨域校验
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+// 处理ws请求
+func WsHandler(w http.ResponseWriter, r *http.Request) {
+	var conn *websocket.Conn
+	var err error
+	conn, err = wsupgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("Failed to set websocket upgrade: %+v", err)
+		return
+	}
+
+	// 必须死循环，gin通过协程调用该handler函数，一旦退出函数，ws会被主动销毁
+	for {
+		// recieve
+		_, reply, err := conn.ReadMessage()
+		if err != nil {
+			break
+		}
+		fmt.Println(string(reply))
+	}
+}
 
 func main() {
 	gin.SetMode(gin.ReleaseMode)
